52a809bdaa6fed12a08f4fa7b1e3085e3fe967a1
Ben <bch46@cornell.edu>
2014-05-13 21:18:11 -0400

    Some basic drawing stuff

--- FogOfWarApp/core/src/fow/app/MasterScreen.java
+++ FogOfWarApp/core/src/fow/app/MasterScreen.java
@@ -1,7 +1,7 @@
 package fow.app;
 
 public class MasterScreen extends AbstractScreen {
-
+	
 	public MasterScreen(Main game) {
 		super(game);
 		NetworkEventListener listener = new NetworkEventListener();
@@ -9,6 +9,12 @@ public class MasterScreen extends AbstractScreen {
 	}
 	
 	private class NetworkEventListener extends HandshakeListener {
+		
+	}
+	
+	@Override
+	public void render(float delta) {
+		super.render(delta);
 		
 	}
 
--- FogOfWarApp/core/src/fow/app/PlayerScreen.java
+++ FogOfWarApp/core/src/fow/app/PlayerScreen.java
@@ -1,14 +1,32 @@
 package fow.app;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class PlayerScreen extends AbstractScreen {
 
+	private Texture texture;
+	
 	public PlayerScreen(Main game) {
 		super(game);
 		NetworkEventListener listener = new NetworkEventListener();
 		game.serverConnection.setOnReceiveNetworkEventListener(listener);
+
+		texture = new Texture(Gdx.files.internal("gnome.gif"));
 	}
 	
 	private class NetworkEventListener extends HandshakeListener {
 		
+	}
+	
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+		
+		SpriteBatch batch = getBatch();
+		batch.begin();
+		batch.draw(texture, 50, 50);
+		batch.end();
 	}
 }
