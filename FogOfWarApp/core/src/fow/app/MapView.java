f950c3e7208db9d392be2cdab3cb3f20f225ab18
Ben <bch46@cornell.edu>
2014-05-21 19:01:58 -0400

    Figured out how to support multiple viewports Also fixed zooming, and made all input handling event based

--- FogOfWarApp/core/src/fow/app/MapView.java
+++ FogOfWarApp/core/src/fow/app/MapView.java
@@ -1,26 +1,29 @@
 package fow.app;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import fow.common.PositionTuple;
 import fow.common.VisibilityLayer;
 
-public class MapView {
+public class MapView extends Stage {
 
+    // Camera constants for pan and zoom speed
+    private static final int CAM_PAN_AMT = 3;
+    private static final float CAM_ZOOM_AMT = 0.05f;
+    
+    // The texture we use for any character on the screen. Future implementations would probably
+    // expand this to different textures for different characters
     private final Texture texture;
 
     // This is essentially the state, as provided by the server
     protected VisibilityLayer visibility;
-
-    /*
-     * Camera fields, storing the dimensions of the camera and its location in world coords.
-     * Location maps to the origin of the camera (bottom left corner)
-     */
-    private Rectangle camViewport;
-    private float camZoom;
 
     /*
      * Represents the current position of your character in world coordinates. This can change if
@@ -30,25 +33,33 @@ public class MapView {
     private int y;
 
     /*
-     * Whether or not the character's avatar is currently being dragged around
+     * Whether or not the character's avatar is currently being dragging around
      */
-    private boolean dragged;
-    
+    private boolean dragging;
+
     private boolean untouched;
+    
+    private Vector2 translation;
+    private float zoomFactor;
 
     /**
      * Default constructor, initializes fields to default values
      */
     public MapView() {
+        super();
+
+        Viewport viewport = new MapViewport();
+        setViewport(viewport);
+
         texture = new Texture(Gdx.files.internal("gnome.gif"));
-        camViewport = new Rectangle();
-        camZoom = 0f;
 
         x = 0;
         y = 0;
 
-        dragged = false;
+        dragging = false;
         untouched = false;
+        translation = new Vector2();
+        zoomFactor = 0f;
     }
 
     /**
@@ -64,98 +75,117 @@ public class MapView {
         y = visibility.getPlayers()[0].path[0].y;
 
         // Try to center the camera on the player
-        setCameraTo(x, y);
-    }
-
-    /**
-     * Set the width and height of the camera's viewport. This should be determined by the size of
-     * the screen, minus the space taken up by the menu.
-     * 
-     * @param width
-     * @param height
-     */
-    public void setCameraBounds(int width, int height) {
-        this.camViewport.setWidth(width).setHeight(height);
-    }
-
-    /**
-     * Tries to center the camera around the point x, y Takes world bounds into consideration
-     * 
-     * @param x coordinate
-     * @param y coordinate
-     */
-    private void setCameraTo(int x, int y) {
-        camViewport.x = x - (int) (camViewport.width / 2);
-        camViewport.y = y - (int) (camViewport.height / 2);
-        if (camViewport.x < 0) {
-            camViewport.x = 0;
-        } else if (camViewport.x + camViewport.width > visibility.getLevelWidth()) {
-            camViewport.x = visibility.getLevelWidth() - camViewport.width;
-        }
-        if (camViewport.y < 0) {
-            camViewport.y = 0;
-        } else if (camViewport.y + camViewport.height > visibility.getLevelHeight()) {
-            camViewport.y = visibility.getLevelHeight() - camViewport.height;
-        }
-    }
-
-    /**
-     * Sets the amount of zoom for the camera. Zoom is currently unimplemented.
-     * 
-     * @param sliderValue zoom value, 1 = full scale, 0 = fully zoomed out
-     */
-    public void setCameraZoom(float sliderValue) {
-        this.camZoom = sliderValue;
+        getCamera().position.set(x, y, 0);
     }
 
     public PositionTuple getCurrentPlayerPosition() {
         return new PositionTuple(x, y);
     }
 
-    public void render(AbstractScreen screen) {
+    @Override
+    public void draw() {
+        super.draw();
+        
+        // Translate camera if necessary
+        OrthographicCamera cam = (OrthographicCamera) getCamera();
+        cam.translate(translation);
+        
+        // Zoom camera if necessary, don't let it zoom too far in or we get backwards world
+        if (cam.zoom + zoomFactor > CAM_ZOOM_AMT) {
+            cam.zoom += zoomFactor;
+        }
+        
+        // Only attempt to draw the world if we have a visibility layer to work from
         if (visibility != null) {
-            handleInput(screen);
-            
-            if (dragged) {
-                int tempy = ((int) screen.stage.getHeight()) - Gdx.input.getY(0);
-                if (tempy > 100) {
-                    x = Gdx.input.getX(0) + (int) camViewport.x;
-                    y = tempy + (int) camViewport.y;
-                }
-                System.out.println("x = " + x + ", y = " + y);
-            }
+            int dx = x - texture.getWidth() / 2;
+            int dy = y - texture.getHeight() / 2;
 
-
-            int dx = x - texture.getWidth() / 2 - (int) camViewport.x;
-            int dy = y - texture.getHeight() / 2 - (int) camViewport.y;
-
-            SpriteBatch batch = screen.getBatch();
+            Batch batch = getSpriteBatch();
             batch.begin();
             batch.draw(texture, dx, dy, texture.getHeight(), texture.getWidth());
             batch.end();
         }
     }
 
-    private void handleInput(AbstractScreen screen) {
-        if (Gdx.input.isTouched(0)) {
-            
-            int halfWidth = texture.getWidth() / 2;
-            int halfHeight = texture.getHeight() / 2;
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        int halfWidth = texture.getWidth() / 2;
+        int halfHeight = texture.getHeight() / 2;
 
-            int tempX = Gdx.input.getX(0);
-            int tempY = ((int) screen.stage.getHeight()) - Gdx.input.getY(0);
-            tempX += (int) camViewport.x;
-            tempY += (int) camViewport.y;
+        Vector2 stageCoords = screenToStageCoordinates(new Vector2(screenX, screenY));
 
-            if (tempX >= x - halfWidth && tempX <= x + halfWidth && tempY >= y - halfHeight
-                    && tempY <= y + halfHeight && untouched) {
-                dragged = true;
-            }
-            
-            untouched = false;
-        } else {
-            dragged = false;
-            untouched = true;
+        if (stageCoords.x >= x - halfWidth && stageCoords.x <= x + halfWidth
+                && stageCoords.y >= y - halfHeight && stageCoords.y <= y + halfHeight && untouched) {
+            dragging = true;
         }
+        untouched = false;
+
+        System.out.println("x = " + screenX + ", y = " + screenY + ", dragging: " + dragging);
+        
+        return super.touchDown(screenX, screenY, pointer, button);
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        if (dragging) {
+            Vector2 stageCoords = screenToStageCoordinates(new Vector2(screenX, screenY));
+            x = (int) stageCoords.x;
+            y = (int) stageCoords.y;
+        }
+
+        return super.touchDragged(screenX, screenY, pointer);
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        dragging = false;
+        untouched = true;
+        return super.touchUp(screenX, screenY, pointer, button);
+    }
+    
+    @Override
+    public boolean keyDown (int keyCode) {
+        if(keyCode == Input.Keys.A) {
+            zoomFactor += CAM_ZOOM_AMT;
+        }
+        if(keyCode ==  Input.Keys.Q) {
+            zoomFactor -= CAM_ZOOM_AMT;
+        }
+        if(keyCode ==  Input.Keys.LEFT) {
+                translation.add(-CAM_PAN_AMT, 0);
+        }
+        if(keyCode ==  Input.Keys.RIGHT) {
+                translation.add(CAM_PAN_AMT, 0);
+        }
+        if(keyCode ==  Input.Keys.DOWN) {
+                translation.add(0, -CAM_PAN_AMT);
+        }
+        if(keyCode ==  Input.Keys.UP) {
+                translation.add(0, CAM_PAN_AMT);
+        }
+        return super.keyDown(keyCode);
+    }
+    
+    @Override
+    public boolean keyUp (int keyCode) {
+        if(keyCode == Input.Keys.A) {
+            zoomFactor -= CAM_ZOOM_AMT;
+        }
+        if(keyCode ==  Input.Keys.Q) {
+            zoomFactor += CAM_ZOOM_AMT;
+        }
+        if(keyCode ==  Input.Keys.LEFT) {
+                translation.add(CAM_PAN_AMT, 0);
+        }
+        if(keyCode ==  Input.Keys.RIGHT) {
+                translation.add(-CAM_PAN_AMT, 0);
+        }
+        if(keyCode ==  Input.Keys.DOWN) {
+                translation.add(0, CAM_PAN_AMT);
+        }
+        if(keyCode ==  Input.Keys.UP) {
+                translation.add(0, -CAM_PAN_AMT);
+        }
+        return super.keyUp(keyCode);
     }
 }
