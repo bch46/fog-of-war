2caedfaa60622c75903d52f15f2f4ef28974bca8
Ben <bch46@cornell.edu>
2014-05-21 20:01:12 -0400

    Now we draw avatars for all characters in the VisibilityLayer Also, dragging doesn't snap the center to the mouse pointer now

--- FogOfWarApp/common/src/fow/common/VisibilityLayer.java
+++ FogOfWarApp/common/src/fow/common/VisibilityLayer.java
@@ -95,4 +95,38 @@ public class VisibilityLayer implements Serializable {
 	public void setPlayers(PlayerState[] players) {
 		this.players = players;
 	}
+	
+	public int getNumPlayers() {
+	    return players.length;
+	}
+	
+	/**
+	 * Gets the current position of the ith player in the players array
+	 * @param i the index in the players array
+	 * @return the desired position
+	 */
+	public PositionTuple getPlayerPosition(int i) {
+	    return players[i].path[0];
+	}
+	
+	/**
+	 * Sets the position of the ith player in the players array
+	 * @param i the index in the players array
+	 * @param pos the desired position
+	 */
+	public void setPlayerPosition(int i, PositionTuple pos) {
+	    // Create a new array to hold the old one, plus another element
+	    PositionTuple[] curPath = players[i].path;
+	    PositionTuple[] newPath = new PositionTuple[curPath.length+1];
+	    
+	    // Add new element at front
+	    newPath[0] = pos;
+	    
+	    // Fill in the old path
+	    for (int j = 0; j < curPath.length; j++) {
+	        newPath[j+1] = curPath[j];
+	    }
+	    
+	    players[i].path = newPath;
+	}
 }
--- FogOfWarApp/core/src/fow/app/MapView.java
+++ FogOfWarApp/core/src/fow/app/MapView.java
@@ -17,7 +17,10 @@ public class MapView extends Stage {
     // Camera constants for pan and zoom speed
     private static final int CAM_PAN_AMT = 3;
     private static final float CAM_ZOOM_AMT = 0.05f;
-    
+
+    // Reference to this stage's camera, but cast to an OrthographicCamera
+    private OrthographicCamera camera;
+
     // The texture we use for any character on the screen. Future implementations would probably
     // expand this to different textures for different characters
     private final Texture texture;
@@ -26,20 +29,14 @@ public class MapView extends Stage {
     protected VisibilityLayer visibility;
 
     /*
-     * Represents the current position of your character in world coordinates. This can change if
-     * the user drags their character's avatar around.
-     */
-    private int x;
-    private int y;
-
-    /*
      * Whether or not the character's avatar is currently being dragging around
      */
     private boolean dragging;
 
-    private boolean untouched;
-    
-    private Vector2 translation;
+    private Vector2 dragStartPoint;
+    private Vector2 dragEndPoint;
+
+    private Vector2 camTranslation;
     private float zoomFactor;
 
     /**
@@ -51,14 +48,14 @@ public class MapView extends Stage {
         Viewport viewport = new MapViewport();
         setViewport(viewport);
 
+        camera = (OrthographicCamera) getCamera();
+
         texture = new Texture(Gdx.files.internal("gnome.gif"));
 
-        x = 0;
-        y = 0;
-
         dragging = false;
-        untouched = false;
-        translation = new Vector2();
+        dragStartPoint = new Vector2();
+        dragEndPoint = new Vector2();
+        camTranslation = new Vector2();
         zoomFactor = 0f;
     }
 
@@ -70,40 +67,48 @@ public class MapView extends Stage {
     public void updateVisibility(VisibilityLayer visibility) {
         this.visibility = visibility;
 
-        // The first player in the list should be this player's character
-        x = visibility.getPlayers()[0].path[0].x;
-        y = visibility.getPlayers()[0].path[0].y;
+        // This player's character should be first in the list
+        PositionTuple pos = visibility.getPlayerPosition(0);
 
         // Try to center the camera on the player
-        getCamera().position.set(x, y, 0);
+        camera.position.set(pos.x, pos.y, 0);
     }
 
     public PositionTuple getCurrentPlayerPosition() {
-        return new PositionTuple(x, y);
+        return visibility.getPlayerPosition(0);
     }
 
     @Override
     public void draw() {
         super.draw();
-        
+
         // Translate camera if necessary
         OrthographicCamera cam = (OrthographicCamera) getCamera();
-        cam.translate(translation);
-        
+        cam.translate(camTranslation);
+
         // Zoom camera if necessary, don't let it zoom too far in or we get backwards world
         if (cam.zoom + zoomFactor > CAM_ZOOM_AMT) {
             cam.zoom += zoomFactor;
         }
-        
+
         // Only attempt to draw the world if we have a visibility layer to work from
         if (visibility != null) {
-            int dx = x - texture.getWidth() / 2;
-            int dy = y - texture.getHeight() / 2;
+            for (int i = 0; i < visibility.getNumPlayers(); i++) {
+                PositionTuple currentPos = visibility.getPlayerPosition(i);
 
-            Batch batch = getSpriteBatch();
-            batch.begin();
-            batch.draw(texture, dx, dy, texture.getHeight(), texture.getWidth());
-            batch.end();
+                int dx = currentPos.x - texture.getWidth() / 2;
+                int dy = currentPos.y - texture.getHeight() / 2;
+
+                if (dragging) {
+                    dx += dragEndPoint.x - dragStartPoint.x;
+                    dy += dragEndPoint.y - dragStartPoint.y;
+                }
+
+                Batch batch = getSpriteBatch();
+                batch.begin();
+                batch.draw(texture, dx, dy, texture.getHeight(), texture.getWidth());
+                batch.end();
+            }
         }
     }
 
@@ -112,25 +117,23 @@ public class MapView extends Stage {
         int halfWidth = texture.getWidth() / 2;
         int halfHeight = texture.getHeight() / 2;
 
-        Vector2 stageCoords = screenToStageCoordinates(new Vector2(screenX, screenY));
+        Vector2 worldCoords = screenToStageCoordinates(new Vector2(screenX, screenY));
+        PositionTuple curPos = visibility.getPlayerPosition(0);
 
-        if (stageCoords.x >= x - halfWidth && stageCoords.x <= x + halfWidth
-                && stageCoords.y >= y - halfHeight && stageCoords.y <= y + halfHeight && untouched) {
+        if (worldCoords.x >= curPos.x - halfWidth && worldCoords.x <= curPos.x + halfWidth
+                && worldCoords.y >= curPos.y - halfHeight && worldCoords.y <= curPos.y + halfHeight) {
             dragging = true;
+            dragStartPoint.set(worldCoords);
+            dragEndPoint.set(worldCoords);
         }
-        untouched = false;
 
-        System.out.println("x = " + screenX + ", y = " + screenY + ", dragging: " + dragging);
-        
         return super.touchDown(screenX, screenY, pointer, button);
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
         if (dragging) {
-            Vector2 stageCoords = screenToStageCoordinates(new Vector2(screenX, screenY));
-            x = (int) stageCoords.x;
-            y = (int) stageCoords.y;
+            dragEndPoint.set(screenToStageCoordinates(new Vector2(screenX, screenY)));
         }
 
         return super.touchDragged(screenX, screenY, pointer);
@@ -138,53 +141,62 @@ public class MapView extends Stage {
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        dragging = false;
-        untouched = true;
+        if (dragging) {
+            PositionTuple curPos = visibility.getPlayerPosition(0);
+            int newX = curPos.x + (int) (dragEndPoint.x - dragStartPoint.x);
+            int newY = curPos.y + (int) (dragEndPoint.y - dragStartPoint.y);
+            visibility.setPlayerPosition(0, new PositionTuple(newX, newY));
+            
+            dragStartPoint.set(0, 0);
+            dragEndPoint.set(0, 0);
+            dragging = false;
+        }
+        
         return super.touchUp(screenX, screenY, pointer, button);
     }
-    
+
     @Override
-    public boolean keyDown (int keyCode) {
-        if(keyCode == Input.Keys.A) {
+    public boolean keyDown(int keyCode) {
+        if (keyCode == Input.Keys.A) {
             zoomFactor += CAM_ZOOM_AMT;
         }
-        if(keyCode ==  Input.Keys.Q) {
+        if (keyCode == Input.Keys.Q) {
             zoomFactor -= CAM_ZOOM_AMT;
         }
-        if(keyCode ==  Input.Keys.LEFT) {
-                translation.add(-CAM_PAN_AMT, 0);
+        if (keyCode == Input.Keys.LEFT) {
+            camTranslation.add(-CAM_PAN_AMT, 0);
         }
-        if(keyCode ==  Input.Keys.RIGHT) {
-                translation.add(CAM_PAN_AMT, 0);
+        if (keyCode == Input.Keys.RIGHT) {
+            camTranslation.add(CAM_PAN_AMT, 0);
         }
-        if(keyCode ==  Input.Keys.DOWN) {
-                translation.add(0, -CAM_PAN_AMT);
+        if (keyCode == Input.Keys.DOWN) {
+            camTranslation.add(0, -CAM_PAN_AMT);
         }
-        if(keyCode ==  Input.Keys.UP) {
-                translation.add(0, CAM_PAN_AMT);
+        if (keyCode == Input.Keys.UP) {
+            camTranslation.add(0, CAM_PAN_AMT);
         }
         return super.keyDown(keyCode);
     }
-    
+
     @Override
-    public boolean keyUp (int keyCode) {
-        if(keyCode == Input.Keys.A) {
+    public boolean keyUp(int keyCode) {
+        if (keyCode == Input.Keys.A) {
             zoomFactor -= CAM_ZOOM_AMT;
         }
-        if(keyCode ==  Input.Keys.Q) {
+        if (keyCode == Input.Keys.Q) {
             zoomFactor += CAM_ZOOM_AMT;
         }
-        if(keyCode ==  Input.Keys.LEFT) {
-                translation.add(CAM_PAN_AMT, 0);
+        if (keyCode == Input.Keys.LEFT) {
+            camTranslation.add(CAM_PAN_AMT, 0);
         }
-        if(keyCode ==  Input.Keys.RIGHT) {
-                translation.add(-CAM_PAN_AMT, 0);
+        if (keyCode == Input.Keys.RIGHT) {
+            camTranslation.add(-CAM_PAN_AMT, 0);
         }
-        if(keyCode ==  Input.Keys.DOWN) {
-                translation.add(0, CAM_PAN_AMT);
+        if (keyCode == Input.Keys.DOWN) {
+            camTranslation.add(0, CAM_PAN_AMT);
         }
-        if(keyCode ==  Input.Keys.UP) {
-                translation.add(0, -CAM_PAN_AMT);
+        if (keyCode == Input.Keys.UP) {
+            camTranslation.add(0, -CAM_PAN_AMT);
         }
         return super.keyUp(keyCode);
     }
